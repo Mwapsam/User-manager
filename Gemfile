0b95ca9b45c7d487fef54a140c93321422054bbf
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-05-22 13:10:11 +0200

    Create pages styles

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ group :test do
 end
 
 gem "devise", "~> 4.8"
+
+gem "importmap-rails", "~> 1.0"
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
